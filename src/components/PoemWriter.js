b88f28fc89f5341c50417d54dafa705829de52e2
aalexander3 <aralx73@gmail.com>
2018-04-04 10:20:26 -0400

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,16 +4,64 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: '',
+      isValid: false
+    };
+  }
+
+  doBoth = (e) => {
+    this.handleTyping(e)
+    this.checkForErrors(e)
+  }
+
+  handleTyping = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  checkForErrors = (e) => {
+    console.log(this.poemValid(e.target.value));
+    console.log(this.state.isValid);
+    if (this.poemValid(e.target.value)) {
+      this.setState({isValid: true})
+    } else {
+      this.setState({isValid: false})
+    }
+  }
+
+  poemValid = (poem) => {
+    const poemLines = poem.split('\n').map(line => line.trim());
+    const lineCheck = poemLines.length === 3;
+
+    if (poem && lineCheck) {
+      return (
+        poemLines[0].split(" ").length === 5 &&
+        poemLines[1].split(" ").length === 3 &&
+        poemLines[2].split(" ").length === 5
+      )
+    } else {
+      return false
+    }
+  }
+
+  showError = () => {
+    return (this.state.isValid) ? "" : "Please restructure into a real poem"
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
-          This poem is not written in the right structure!
-        </div>
+        <textarea rows="3" cols="60" onChange={this.doBoth} value={this.state.value} />
+
+        {(!this.state.isValid) ?
+          <div id="poem-validation-error" style={{ color: "red" }}>
+            {this.showError()}
+          </div> : null
+        }
+
       </div>
     );
   }
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,33 @@
 import React from "react";
 
+let remainingCharacters = 140;
+
+
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: '',
+      maxChars: 140
+    };
+  }
+
+  handleTweet = (e) => {
+    console.log(e.target.value)
+
+    this.setState({
+      value: e.target.value,
+      maxChars: 140 - e.target.value.length
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <p>Characters Remaining: {this.state.maxChars} </p>
+        <input type="text" value={this.state.value} onChange={this.handleTweet} />
       </div>
     );
   }
