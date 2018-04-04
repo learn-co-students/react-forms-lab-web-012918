403a515da29a83603d329874a07247c2e9d25822
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-04-04 17:20:02 -0400

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,18 +1,50 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      poemInput: '',
+      errorMessage: 'This poem is not written in the right structure!'
+    };
+  }
+
+  handlePoemChange = event => {
+    this.setState({
+      poemInput: event.target.value
+    })
+    let poemLines = this.state.poemInput.split('\n')
+    if (poemLines.length === 3){
+      let line1 = poemLines[0].split(' ')
+      let line2 = poemLines[1].split(' ')
+      let line3 = poemLines[2].split(' ')
+      if( line1.length === 5 && line2.length === 3 && line3.length === 5 ){
+        this.setState({errorMessage: ''})
+        console.log(this.state)
+
+      }else {
+        this.setState({errorMessage: 'This poem is not written in the right structure!'})
+        console.log(this.state)
+      }
+    }
+
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
+        <textarea
+          rows="3"
+          cols="60"
+          id='poem'
+          value={this.state.poemInput}
+          onChange={this.handlePoemChange}
+        />
+      <h3>Poem Test: {this.state.poemInput} </h3>
         <div id="poem-validation-error" style={{ color: "red" }}>
-          This poem is not written in the right structure!
+          {this.state.errorMessage}
         </div>
       </div>
     );
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,50 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {messageInput: ''};
+    // this sets the initial state by taking the value were trying to find and setting it up
   }
+
+  handleChange = event => {
+    this.setState({
+      messageInput: event.target.value
+    })
+  }
+
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input
+          type="text"
+          value={this.state.messageInput}
+          onChange={this.handleChange}
+        />
+      <h1>characters remaining</h1>
+      <h3>{this.props.maxChars - this.state.messageInput.length}</h3>
       </div>
     );
+    // subtracts input length with the prop of maxChars which is 140 by default
   }
+
 }
+
+TwitterMessage.propTypes = {
+  maxChars: PropTypes.number,
+};
+// validates that the prop is a number
+
+TwitterMessage.defaultProps = {
+  maxChars: 140,
+};
+// makes default of prop 140
+
+//proptypes should be outside of the class
 
 export default TwitterMessage;
