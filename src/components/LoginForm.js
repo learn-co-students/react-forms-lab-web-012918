9102abde00a50acf287f025f9551867caf249f49
github username <diep.christopher@gmail.com>
2018-04-04 22:46:34 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,52 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleUsernameInput = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  handlePasswordInput = (event) => {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    //why set this?
+    const {username, password} = this.state
+
+    //this conditional ends the function?
+    if (username === '' || password === '') {
+      return
+    }
+
+    //I had problems with this.props.onSubmit It was undefined. Also had trouble passing in arguments.
+    this.props.onSubmit({username, password})
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" value={this.state.username} onChange={this.handleUsernameInput}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" value={this.state.password} onChange={this.handlePasswordInput}/>
           </label>
         </div>
         <div>
--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,16 +4,51 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      poem: '',
+      valid: false
+    };
   }
+
+  validator = (content) => {
+    //split into an array of lines and make it an array that cuts off extra space
+    const contentLines = content.split("\n").map(line => line.trim());
+
+    //check if the array has three lines
+    const threeLinesCheck = contentLines.length === 3;
+
+    //if three lines exist then check whether each line has the right # of words
+    if (threeLinesCheck) {
+      return (
+        contentLines[0].split(' ').length === 5 &&
+        contentLines[1].split(' ').length === 3 &&
+        contentLines[2].split(' ').length === 5
+      )
+    } else {
+      return false
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      poem: event.target.value,
+      valid: this.validator(event.target.value)//update this with a method or function
+    })
+    //const errorDiv = event.target.parentElement.children[1]
+
+  }
+
+
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
+        <textarea rows="3" cols="60" value={this.state.poem} onChange={this.handleChange}/>
+        {!this.state.valid ? (
         <div id="poem-validation-error" style={{ color: "red" }}>
           This poem is not written in the right structure!
         </div>
+      ) : null}
       </div>
     );
   }
